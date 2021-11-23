8533c8dcb0ee624c89efa6546c13be35f01f491b
c1m50c <terminalityms@gmail.com>
2021-11-23 15:47:12 -0600

    Quadratic Variation 📈

--- src/main.py
+++ src/main.py
@@ -8,9 +8,13 @@ START_TIME: float = 0.0
 FINAL_TIME: float = 1.0
 
 
+def quadratic_variation(brownian_motion) -> np.ndarray:
+    return np.cumsum(
+        a=np.power(np.diff(a=brownian_motion, axis=0, prepend=0.0), 2), axis=0
+    )
 
 
-def main():
+def main() -> None:
     times: np.array = np.linspace(start=START_TIME, stop=FINAL_TIME, num=SAMPLE_POINTS)
     t_step: float = times[1] - times[0]
     
